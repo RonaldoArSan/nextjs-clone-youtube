23ed2b6f46e7ddb521225c8f56c6e408af5ac328
Ronaldo Arsan <ronaldoarsan@gmail.com>
2021-06-30 22:10:51 -0300

    Material-UI

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Button } from '@material-ui/core';
 
 export default function Home() {
   return (
     <div>
-      Clone youtubeclone
-
+     
+      Clone you tube clone
+      <Button variant="contained" color="primery">
+  Primary
+</Button>
     </div>
   );
 }
